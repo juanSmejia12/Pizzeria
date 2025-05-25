7ce916fe9d29fbc1cc55837ec656b1d7abf78874
cristian camilo lópez giraldo <k-milo3110@hotmail.es>
2025-05-24 20:49:19 -0500

    routas api de branch y client

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\BranchController;
+use App\Http\Controllers\api\ClientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/branches', [BranchController::class, 'store'])->name('branches.store');
+Route::get('/branches', [BranchController::class, 'index'])->name('branches');
+Route::delete('/branches/{branch}', [BranchController::class, 'destroy'])->name('branches.destroy');
+Route::get('/branches/{branch}', [BranchController::class, 'show'])->name('branches.show');
+Route::put('/branches/{branch}', [BranchController::class, 'update'])->name('branches.update');
+
+Route::post('/clients', [BranchController::class, 'store'])->name('clients.store');
+Route::get('/clients', [BranchController::class, 'index'])->name('clients');
+Route::delete('/clients/{client}', [BranchController::class, 'destroy'])->name('branches.destroy');
+Route::get('/clients/{client}', [BranchController::class, 'show'])->name('clients.show');
+Route::put('/clients/{client}', [BranchController::class, 'update'])->name('clients.update');
