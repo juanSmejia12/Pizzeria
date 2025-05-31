5f24dfbc18b9824b018069e83c5f8027522cea60
JuanMejia <juansebastianmejiagomez31@gmail.com>
2025-05-31 16:48:47 -0500

    Forzando commit por cambios en line endings

--- resources/views/layouts/Guest.blade.php
+++ resources/views/layouts/Guest.blade.php
@@ -21,27 +21,30 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('employees.index') }}">Empleados</a>
                 </li>
-                <li class="nav-item">
+<<<<<<< HEAD
+=======
+                {{-- <li class="nav-item">
                     <a class="nav-link" href="{{ route('extra-ingredients.index') }}">Ingredientes Extra</a>
-                </li>
+                </li> --}}
+>>>>>>> mejia
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('ingredients.index') }}">Ingredientes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('orders.index') }}">Órdenes</a>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link" href="{{ route('pizzas.index') }}">Pizzas</a>
                 </li>
-                <li class="nav-item">
+<<<<<<< HEAD
+=======
+                {{--<li class="nav-item">
                     <a class="nav-link" href="{{ route('purchases.index') }}">Compras</a>
-                </li>
-                <li class="nav-item">
+                </li>--}}
+                {{--<li class="nav-item">
                     <a class="nav-link" href="{{ route('suppliers.index') }}">Proveedores</a>
-                </li>
+                </li>--}}
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('profile.edit') }}">Perfil</a>
                 </li>
+>>>>>>> mejia
             </ul>
         </div>
     </nav>
